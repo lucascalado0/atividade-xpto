5b6e29ab189d048f6f682126a1e397b5780c7c02
devlucascalado <devlucascalado@gmail.com>
2025-10-20 18:59:49 -0300

    first commit

---
+++ src/main/java/com/example/atividade_xpto/AtividadeXptoApplication.java
@@ -0,0 +1,13 @@
+package com.example.atividade_xpto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AtividadeXptoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AtividadeXptoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/atividade_xpto/AtividadeXptoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.atividade_xpto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AtividadeXptoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
