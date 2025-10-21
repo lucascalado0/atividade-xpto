49fb5b22c65d23d7868ccbf2883ea59dfae06372
devlucascalado <devlucascalado@gmail.com>
2025-10-20 23:38:23 -0300

    classe endereco

---
+++ src/main/java/com/example/atividade_xpto/model/Endereco.java
@@ -0,0 +1,44 @@
+package com.example.atividade_xpto.model;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@ToString
+@Table(name = "tb_enderecos")
+public class Endereco {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, length = 12)
+    private String cep;
+
+    @Column(nullable = false, length = 30)
+    private String rua;
+
+    @Column(nullable = false, length = 4)
+    private String numero;
+
+    @Column(nullable = false, length = 30)
+    private String bairro;
+
+    @Column(nullable = false, length = 30)
+    private String cidade;
+
+    @Column(nullable = false, length = 30)
+    private String estado;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "cliente_id")
+    @ToString.Exclude
+    private Cliente cliente;
+
+
+}
