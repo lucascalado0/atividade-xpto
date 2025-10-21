5423ccbc5452ec3913ecc8cc1c02e832dec7ae43
devlucascalado <devlucascalado@gmail.com>
2025-10-20 23:37:59 -0300

    classe clientepj

---
+++ src/main/java/com/example/atividade_xpto/model/ClientePJ.java
@@ -0,0 +1,21 @@
+package com.example.atividade_xpto.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.DiscriminatorValue;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@ToString(callSuper = true)
+@Entity
+@Table(name = "tb_clientes_pj")
+@DiscriminatorValue("PJ")
+public class ClientePJ extends Cliente {
+
+    @Column(nullable = false, unique = true, length = 20)
+    private String cnpj;
+}
