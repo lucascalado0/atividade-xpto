8011b2bc5e361fc1a89a888aab930c475c545e13
devlucascalado <devlucascalado@gmail.com>
2025-10-22 15:05:46 -0300

    repositories

--- src/main/java/com/example/atividade_xpto/repository/ClienteRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.atividade_xpto.repository;
-
-import com.example.atividade_xpto.core.models.Cliente;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ClienteRepository extends JpaRepository<Cliente, Long> {
-}
