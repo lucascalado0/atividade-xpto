f92a58276b89c961a81e6a03ea0ef91e3b7338ef
devlucascalado <devlucascalado@gmail.com>
2025-10-21 23:32:32 -0300

    conta service

--- src/main/java/com/example/atividade_xpto/service/ContaService.java
+++ src/main/java/com/example/atividade_xpto/service/ContaService.java
@@ -1,16 +1,56 @@
 package com.example.atividade_xpto.service;
 
+import com.example.atividade_xpto.core.models.Cliente;
+import com.example.atividade_xpto.core.models.Conta;
+import com.example.atividade_xpto.exception.clientes.ClienteNotFoundException;
+import com.example.atividade_xpto.exception.contas.ContaNotFoundException;
+import com.example.atividade_xpto.repository.ClienteRepository;
 import com.example.atividade_xpto.repository.ContaRepository;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class ContaService {
 
     private final ContaRepository contaRepository;
+    private final ClienteRepository clienteRepository;
 
-    public ContaService(ContaRepository contaRepository) {
+
+    public ContaService(ContaRepository contaRepository, ClienteRepository clienteRepository) {
+        this.clienteRepository = clienteRepository;
         this.contaRepository = contaRepository;
     }
 
-    
+    public Conta novaConta(Conta conta, Long clienteId) {
+        Cliente cliente =  clienteRepository.findById(clienteId)
+                .orElseThrow(() -> new ClienteNotFoundException("Cliente com ID " + clienteId + " não encontrado."));
+
+        conta.setCliente(cliente);
+
+        return contaRepository.save(conta);
+    }
+
+    public Conta obterContaPorId(Long id) {
+        return contaRepository.findById(id)
+                .orElseThrow(() -> new ContaNotFoundException("Conta com ID " + id + " não encontrada."));
+    }
+
+    public List<Conta> listarContas() {
+        return contaRepository.findAll();
+    }
+
+    public Conta atualizarConta(Long id, Conta contaAtualizada) {
+        Conta contaExistente = obterContaPorId(id);
+
+        contaExistente.setSaldo(contaAtualizada.getSaldo());
+
+        return contaRepository.save(contaExistente);
+    }
+
+    public void deletarContaPorId(Long id) {
+        Conta conta = obterContaPorId(id);
+        contaRepository.delete(conta);
+    }
+
 }
