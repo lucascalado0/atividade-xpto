87cebc7346eac4aa3b55b7a2af7abf0570bd9b9b
devlucascalado <devlucascalado@gmail.com>
2025-10-22 15:06:30 -0300

    movimentacao repository

---
+++ src/main/java/com/example/atividade_xpto/repository/movimentacao/MovimentacaoRepository.java
@@ -0,0 +1,15 @@
+package com.example.atividade_xpto.repository.movimentacao;
+
+import com.example.atividade_xpto.core.models.movimentacao.Movimentacao;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Repository
+public interface MovimentacaoRepository extends JpaRepository<Movimentacao, Long> {
+
+    List<Movimentacao> findByContaIdAndDataMovimentacaoBetween(Long contaId, LocalDateTime dataInicio, LocalDateTime dataFim);
+
+}
