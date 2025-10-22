0576857febf1050a763a4f0e53ce833c2d739b85
devlucascalado <devlucascalado@gmail.com>
2025-10-22 19:55:39 -0300

    main

--- src/main/java/com/example/atividade_xpto/AtividadeXptoApplication.java
+++ src/main/java/com/example/atividade_xpto/AtividadeXptoApplication.java
@@ -1,10 +1,12 @@
 package com.example.atividade_xpto;
 
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
 @SpringBootApplication
-public class AtividadeXptoApplication {
+public class AtividadeXptoApplication{
 
 	public static void main(String[] args) {
 		SpringApplication.run(AtividadeXptoApplication.class, args);
