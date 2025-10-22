d864074c87dbaeaba5cdf9aa9fe8ed50fb260b01
devlucascalado <devlucascalado@gmail.com>
2025-10-22 15:05:06 -0300

    endereco entidade

---
+++ src/main/java/com/example/atividade_xpto/core/models/endereco/Endereco.java
@@ -0,0 +1,41 @@
+package com.example.atividade_xpto.core.models.endereco;
+
+import com.example.atividade_xpto.core.models.clientes.Cliente;
+import com.fasterxml.jackson.annotation.JsonBackReference;
+import jakarta.persistence.*;
+import lombok.*;
+
+
+@Data
+@Entity
+@Table(name = "tb_enderecos")
+public class Endereco {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, length = 12)
+    private String cep;
+
+    @Column(nullable = false, length = 30)
+    private String rua;
+
+    @Column(nullable = false, length = 4)
+    private String numero;
+
+    @Column(nullable = false, length = 30)
+    private String bairro;
+
+    @Column(nullable = false, length = 30)
+    private String cidade;
+
+    @Column(nullable = false, length = 30)
+    private String estado;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "cliente_id")
+    @ToString.Exclude
+    @JsonBackReference
+    private Cliente cliente;
+}
