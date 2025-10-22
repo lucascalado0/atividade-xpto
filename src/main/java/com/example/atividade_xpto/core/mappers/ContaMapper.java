21a0344be6c71a9e0bdeb9da7421bf7ddc88b8ae
devlucascalado <devlucascalado@gmail.com>
2025-10-21 23:32:17 -0300

    conta mapper

--- src/main/java/com/example/atividade_xpto/core/mappers/ContaMapper.java
+++ src/main/java/com/example/atividade_xpto/core/mappers/ContaMapper.java
@@ -2,11 +2,17 @@ package com.example.atividade_xpto.core.mappers;
 
 import com.example.atividade_xpto.core.dtos.contas.ContaDTO;
 import com.example.atividade_xpto.core.models.Conta;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 
 import java.util.List;
 
+@Mapper(componentModel = "spring")
 public interface ContaMapper {
+    @Mapping(target = "cliente", ignore = true)
     Conta toEntity(ContaDTO dto);
+
     ContaDTO toDTO(Conta entity);
+
     List<ContaDTO> toDTOList(List<Conta> entities);
 }
