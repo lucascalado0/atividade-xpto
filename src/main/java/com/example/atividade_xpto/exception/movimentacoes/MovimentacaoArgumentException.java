dcdb7a0e2cf5046383001630fefb21108a4d19c6
devlucascalado <devlucascalado@gmail.com>
2025-10-22 15:05:33 -0300

    movimentacao exceptions

---
+++ src/main/java/com/example/atividade_xpto/exception/movimentacoes/MovimentacaoArgumentException.java
@@ -0,0 +1,7 @@
+package com.example.atividade_xpto.exception.movimentacoes;
+
+public class MovimentacaoArgumentException extends RuntimeException {
+    public MovimentacaoArgumentException(String message) {
+        super(message);
+    }
+}
---
+++ src/main/java/com/example/atividade_xpto/exception/movimentacoes/MovimentacaoNotFoundException.java
@@ -0,0 +1,7 @@
+package com.example.atividade_xpto.exception.movimentacoes;
+
+public class MovimentacaoNotFoundException extends RuntimeException {
+    public MovimentacaoNotFoundException(String message) {
+        super(message);
+    }
+}
