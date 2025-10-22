5dec271cc69c2ba0c746c5737b777cfb57a0180b
devlucascalado <devlucascalado@gmail.com>
2025-10-22 15:06:02 -0300

    cliente-pj repository

---
+++ src/main/java/com/example/atividade_xpto/repository/clientes/ClientePJRepository.java
@@ -0,0 +1,16 @@
+package com.example.atividade_xpto.repository.clientes;
+
+import com.example.atividade_xpto.core.models.clientes.ClientePJ;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface ClientePJRepository extends JpaRepository<ClientePJ, Long> {
+    Optional<ClientePJ> findByCnpj(String cnpj);
+    boolean existsByCnpj(String cnpj);
+    @Modifying
+    void deleteByCnpj(String cnpj);
+}
