9947b7a44ac03883265516e0d1df683d4005a689
devlucascalado <devlucascalado@gmail.com>
2025-10-22 15:03:13 -0300

    endereco dto

---
+++ src/main/java/com/example/atividade_xpto/core/dtos/endereco/EnderecoDTO.java
@@ -0,0 +1,31 @@
+package com.example.atividade_xpto.core.dtos.endereco;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import jakarta.validation.constraints.NotBlank;
+import lombok.Data;
+
+@Data
+public class EnderecoDTO {
+    private Long id;
+
+    @NotBlank
+    private String cep;
+
+    @NotBlank
+    private String rua;
+
+    @NotBlank
+    private String numero;
+
+    @NotBlank
+    private String bairro;
+
+    @NotBlank
+    private String cidade;
+
+    @NotBlank
+    private String estado;
+
+    @JsonProperty("cliente_id")
+    private Long clienteId;
+}
