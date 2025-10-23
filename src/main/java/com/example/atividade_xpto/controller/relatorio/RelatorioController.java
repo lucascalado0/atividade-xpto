8791c457f859835e48518ef3c52bfae14a8f15a0
devlucascalado <devlucascalado@gmail.com>
2025-10-22 22:00:14 -0300

    relatorio controller

--- src/main/java/com/example/atividade_xpto/controller/relatorio/RelatorioController.java
+++ src/main/java/com/example/atividade_xpto/controller/relatorio/RelatorioController.java
@@ -1,11 +1,11 @@
 package com.example.atividade_xpto.controller.relatorio;
 
 import com.example.atividade_xpto.service.relatorio.RelatorioService;
+import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.time.LocalDate;
 
 @RestController
 @RequestMapping("/relatorios")
@@ -27,6 +27,16 @@ public class RelatorioController {
     @GetMapping("/saldo/todos")
     public ResponseEntity<String> gerarRelatorioTodos() {
         String relatorio = relatorioService.gerarRelatorioTodosClientes();
+        return ResponseEntity.ok(relatorio);
+    }
+
+    @GetMapping("/saldo/{clienteId}/periodo")
+    public ResponseEntity<String> gerarRelatorioPeriodo(
+            @PathVariable Long clienteId,
+            @RequestParam @DateTimeFormat(pattern = "dd/MM/yyyy") LocalDate dataInicio,
+            @RequestParam @DateTimeFormat(pattern = "dd/MM/yyyy") LocalDate dataFim) {
+
+        String relatorio = relatorioService.gerarRelatorioSaldoPorPeriodo(clienteId, dataInicio, dataFim);
         return ResponseEntity.ok(relatorio);
     }
 }
